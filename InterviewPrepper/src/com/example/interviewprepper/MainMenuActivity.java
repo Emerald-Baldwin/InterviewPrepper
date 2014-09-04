14457a22a067b7592e27ae1db3ed17d1c9604beb
Emerald-Baldwin <baldwier@plu.edu>
2014-09-03 19:30:49 -0700

    Add main menu skeleton
    -Change opening app to the main menu activity
    -Link questions view to "Begin Interview" button

---
+++ InterviewPrepper/src/com/example/interviewprepper/MainMenuActivity.java
@@ -0,0 +1,40 @@
+package com.example.interviewprepper;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainMenuActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_menu);
+		
+		Button beginInterviewButton = (Button) findViewById(R.id.begin);
+		beginInterviewButton.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(MainMenuActivity.this, MainActivity.class);
+				startActivity(i);	
+			}
+			
+		});
+		
+		// todo implement settings button after settings view is created
+		Button settingsButton = (Button) findViewById(R.id.settings);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_menu, menu);
+		return true;
+	}
+
+}
